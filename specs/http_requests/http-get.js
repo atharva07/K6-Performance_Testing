1f697db28126eafdea2037df7002509ed1d661d0
atharva07 <atharva.hiwase07@gmail.com>
2025-03-10 22:12:14 +0530

    http get request with checks

--- specs/http_requests/http-get.js
+++ specs/http_requests/http-get.js
@@ -1,6 +1,15 @@
 import http from 'k6/http'
+import { check } from 'k6';
 
 export default function() {
-    const res = http.get('https://test-api.k6.io/public/crocodiles/');
-    console.log(res);
+    let res = http.get('https://test-api.k6.io/public/crocodiles/');
+    //console.log(res);
+
+    res = http.get('https://test-api.k6.io/public/crocodiles/1/');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'Crocodile is Bert': (r) => r.body.includes('Bert'),
+        'Crocodile is Bert': (r) => r.json().name === 'Bert'
+    })
 }
